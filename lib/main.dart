44a0e48cd6fdbfc31bff8cac247a176086274d3d
민경호 <1736s@naver.com>
2025-02-06 16:29:06 +0900

    feat: 스플래시 스크린이 3초 보인 후 시작화면 보여주도록 설정
    - 추후 시작 화면이 나오기 전 실행할 코드가 있다면 해당 부분에 넣어주면 됨.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print('pausing...');
+    await Future.delayed(const Duration(seconds: 3));
+    print('unpausing...');
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
